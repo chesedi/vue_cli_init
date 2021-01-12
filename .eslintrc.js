9bec2df2f522c492b7affb85bbbe4223bf503c0b
chesedi <a02t02@gmail.com>
2021-01-12 17:01:33 +0900

    eslint & prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,22 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint"
   },
-  rules: {
+  rules : {
+    // "no-console" : "off",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "prettier/prettier": ['error', {
+      singleQuote: true,
+      semi: true,
+      useTabs: true,
+      tabWidth: 2,
+      trailingComma: 'all',
+      printWidth: 80,
+      bracketSpacing: true,
+      arrowParens: 'always'
+    }]
+  },
+  "globals": {
+    // "rMateChartH5": "writable",
   }
 };
